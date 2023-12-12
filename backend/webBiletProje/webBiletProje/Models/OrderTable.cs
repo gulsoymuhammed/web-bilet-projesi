078f524d5f73f53d00553d15208bc8c67282cce1
dezarto <74088316+dezarto@users.noreply.github.com>
2023-12-13 00:12:32 +0300

    backend

---
+++ backend/webBiletProje/webBiletProje/Models/OrderTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webBiletProje.Models
+{
+    public class OrderTable
+    {
+        public int OrderID { get; set; }
+        public int UserId { get; set; }
+        public string OrderDate { get; set; }
+        public int TicketId { get; set; }
+    }
+}
