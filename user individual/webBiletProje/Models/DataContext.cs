30dfda68679e07db1681d8a0c1913a1e76db65e3
suedaklz <suedakalaz@gmail.com>
2023-12-28 00:51:16 +0300

    individual accounts ile backend

---
+++ user individual/webBiletProje/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace webBiletProje.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("biletDataConnection")
+        {
+
+        }
+        //public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<OrderTable> OrderTables { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
